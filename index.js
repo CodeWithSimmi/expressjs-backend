f1a1d5b78843f3a2167264f389bc2a4f82977f70
CodeWithSimmi <arorasimmi1999@gmail.com>
2025-01-31 13:19:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express =require("express")
+const app = express()
+app.use(express.json())
+
+
+
+// req = request , res = response
+app.get("/get-all-contact-details",(req,res)=>{
+    res.json([1,2,3])
+})
+
+app.post("/post-contact-details",(req,res)=>{
+ 
+    const body = req.body ;
+    console.log(body);
+    res.json({
+        ...body
+    });
+
+    res.end();
+})
+
+
+app.listen(4000,()=>{
+    console.log("listing on port 4000")
+})
+
