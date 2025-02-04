08ab6d6a744ce4faf33a845bd5e67d7f9c63feb3
CodeWithSimmi <arorasimmi1999@gmail.com>
2025-02-04 14:40:53 +0530

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # expressjs-backend
+
+// package.json mai change kroge to server ko termintate krege ctrl +c and then use command npm run dev for server start
--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const express =require("express")
-const app = express()
-const cors = require("cors")
-app.use(express.json())
-app.use(cors())
+// including express module and initialising an app
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
+//built middleware  express.json()
+app.use(express.json()); // load in to application
+app.use(cors());
+
+// middleware file export
+const middleware = require("./middleware");
+app.use("/midd", middleware);
+
+//approute file export
+const approute = require("./approute");
+app.use("/apps", approute);
+
+//array file export
+const array = require("./array");
+app.use("/array", array);
 
 // req = request , res = response
-app.get("/get-all-contact-details",(req,res)=>{
-    res.json([1,2,3])
-})
+app.get("/get-all-contact-details", (req, res) => {
+  res.json([1, 2, 3]);
+});
 
-app.post("/post-contact-details",(req,res)=>{
- 
-    const body = req.body ;
-    console.log(body);
-    res.json({
-        ...body
-    });
+app.post("/post-contact-details", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.json({
+    ...body,
+  });
 
-    res.end();
-})
+  res.end();
+});
 
+// start your app or server
+// listen have two argument (port number and show the message when your app start
 
-app.listen(4000,()=>{
-    console.log("listing on port 4000")
-})
-
-
+app.listen(4000, () => {
+  console.log("listing on port 4000");
+});
